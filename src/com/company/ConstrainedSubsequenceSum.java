582a9398b166131721bd7be852aa8323d2c9b0af
zhaoli86 <zhaoli86@gmail.com>
2021-01-02 22:51:50 -0500

    stuff

---
+++ src/com/company/ConstrainedSubsequenceSum.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ConstrainedSubsequenceSum {
+
+    public int constrainedSubsetSum(int[] A, int k) {
+        int res = A[0];
+        Deque<Integer> q = new ArrayDeque<>();
+        for (int i = 0; i < A.length; ++i) {
+            A[i] += !q.isEmpty() ? q.peek() : 0;
+            res = Math.max(res, A[i]);
+            while (!q.isEmpty() && A[i] > q.peekLast())
+                q.pollLast();
+            if (A[i] > 0)
+                q.offer(A[i]);
+            if (i >= k && !q.isEmpty() && q.peek() == A[i - k])
+                q.poll();
+        }
+        return res;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    var input=new int[]{10,2,-10,5,20};
+	    int k=2;
+	    var result = new ConstrainedSubsequenceSum().constrainedSubsetSum(input, k);
+    }
+}
